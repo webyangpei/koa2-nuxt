f34ad885054bf5d20572bd39106c8b9fb7c57fc7
Arche <1208812362@qq.com>
2019-04-29 15:08:29 +0800

    释放API请求响应； 加载ant-design-vue组件库

--- plugins/antd.js
+++ plugins/antd.js
@@ -1,8 +1,7 @@
 import Vue from 'vue'
 import Antd from 'ant-design-vue'
-// import locale from 'ant-design-vue/dist/antd.css'
+import locale from 'ant-design-vue/dist/antd.css'
 
 export default () => {
-  // Vue.use(Antd, { locale })
-  Vue.use(Antd)
+  Vue.use(Antd, { locale })
 }
